c04bc2527350e17396fff62a5361a387acb6793a
ilzam <ilzammulkhaq85@gmail.com>
2024-07-22 23:47:16 +0700

    NYOCOT

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom"
+
 export const Layout = () => {
     return (
         <>
             {/* <Header />
             <Outlet />
             <Footer /> */}
+            <Outlet />
         </>
     )
 }
