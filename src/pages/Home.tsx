f099027cfbb043ea5c0f04b759f7fed089b24220
ilzam <ilzammulkhaq85@gmail.com>
2024-07-22 23:54:30 +0700

    NYOCOT

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,25 @@ export const Home = () => {
             <p>you just paid me 500K for create this site not the revision</p>
             <p>i just ask you to 'sabar' and 'say thank you' to me</p>
             <p>once you chat my number, and gently say 'thank you' and sorry i will make this site work again</p>
-            
+            <p>mbak, km perlu ngerti ngga ada ceritanya org pemborong rumah, rumah nya udh jadi, suruh benerin trs ngga di bayar kan?</p>
+            <p>revisian anda dari saya itu sudah termasuk kemanusiaan, last change even not my responsibility, anda udh mindah tangan codingan ini</p>
+            <p>just ask her, not me, tapi anda ngeyel, y udh kemanusiaan lagi gua bantu, eh nyepam call dan kata - kata ngga enak dung</p>
+            <p>just say thank you, and sorry, aku juga bakal say sorry karena terpaksa kasih paham elu seperti ini</p>
+                
+            <ul>
+                <li>
+                    x : joki lain, revisi kok nggak bayar?
+                    y : silahkan cari joki lain saja, gpp, ini codingan bisa saya serahkan, jangan kembali ke saya kalau ada apa - apa
+                </li>
+                <li>
+                    x : kan itu tanggung jawab mu?
+                    y : sejak kapan kuli kudu benerin rumah yang udh dia bangun tanpa di bayar?
+                </li>
+                <li>
+                    x : kan dari hari jum'at kemarin?
+                    y : bro, this is free service, pengen cepet? make it paid then
+                </li>
+            </ul>
         </>
     )
 }
