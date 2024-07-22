474e55d67c2889a4d0ce398302dd618a8d78e592
ilzam <ilzammulkhaq85@gmail.com>
2024-07-22 23:45:50 +0700

    NYOCOT

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,9 @@
-import { Footer } from "@/partials/Footer"
-import { Header } from "@/partials/Header"
-import { Outlet } from "react-router-dom"
-
 export const Layout = () => {
     return (
         <>
-            <Header />
+            {/* <Header />
             <Outlet />
-            <Footer />
+            <Footer /> */}
         </>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,8 @@ export const Home = () => {
             <p>you can just say thank you and sorry for your annoying call</p>
             <p>you just paid me 500K for create this site not the revision</p>
             <p>i just ask you to 'sabar' and 'say thank you' to me</p>
+            <p>once you chat my number, and gently say 'thank you' and sorry i will make this site work again</p>
+            
         </>
     )
 }
