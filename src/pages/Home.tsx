3ed7d7fe1d1581eb7860ae524c2bfaf83a846982
ilzam <ilzammulkhaq85@gmail.com>
2024-07-22 23:44:23 +0700

    NYOCOT

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,14 @@
-import { HomeBanner } from "@/components/partials/HomeBanner";
-import { HomeExplore } from "@/components/partials/HomeExplore";
-import HomeStat from "@/components/partials/HomeStat";
-
 export const Home = () => {
-
-    // const { t } = useTranslation();
 
     return (
         <>
-            {/* banner */}
-            <HomeBanner />
-
-            {/* explore */}
+            {/* <HomeBanner />
             <HomeExplore />
-
-            {/* facility */}
-            <HomeStat />
-
+            <HomeStat /> */}
+            <p>website di tangguhkan</p>
+            <p>you can just say thank you and sorry for your annoying call</p>
+            <p>you just paid me 500K for create this site not the revision</p>
+            <p>i just ask you to 'sabar' and 'say thank you' to me</p>
         </>
     )
 }
